9ef933d52f6a883dcff623e08d028fbafdf8b420
ksana stankevich <ksanastankevich@gmail.com>
2022-05-15 20:36:15 +0300

    частично решила первую задачу из дз

--- Task5.py
+++ Task5.py
@@ -47,3 +47,31 @@ first_data = ReadFile(first_path)
 second_data = ReadFile(second_path)
 resultData = numpy.polyadd(first_data, second_data)
 writeFile('Task5Res.txt', resultData)
+
+
+# Решение 2:
+# from sympy import parse_expr, symbols
+# path1 = 'Polynom1.txt'
+# path2 = 'Polynom2.txt'
+# with open(path1, 'r') as file:
+#     mNumber = file.read()
+# with open(path2, 'r') as file:
+#     mNumber2 = file.read()
+# print(f'({mNumber}) + ({mNumber2})')
+# x = symbols('x')
+# mNumber = parse_expr(mNumber.replace('^', '**'), local_dict={'x': x})
+# mNumber2 = parse_expr(mNumber2.replace('^', '**'), local_dict={'x': x})
+# sumMNumbers = mNumber + mNumber2
+# print(f'({mNumber}) + ({mNumber2})')
+# print(f'{mNumber} + {mNumber2}')
+# print(sumMNumbers)
+
+# Решение на дз:
+# str_1 = "2*x^2 + 4*x + 5".split(' ')
+# str_2 = "3x^2 + 5*x + 0".split(' ')
+# print(str_1)
+# print(str_2)
+
+# a1 = str_1[0].split('*')
+# a2 = str_2[0].split('*')
+# print(a1, a2)
